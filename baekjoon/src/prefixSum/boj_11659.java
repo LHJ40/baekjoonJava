cea2b95cef1b5a9d2a43ff872bbff79152186234
LHJ <puppy970520@naver.com>
2024-03-15 01:27:30 +0900

    prefix

---
+++ baekjoon/src/prefixSum/boj_11659.java
@@ -0,0 +1,27 @@
+package prefixSum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_11659 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		int N=Integer.parseInt(st.nextToken());
+		int M=Integer.parseInt(st.nextToken());
+		int prefix[]=new int[N+1];
+		st= new StringTokenizer(br.readLine());
+		for(int i=1; i<N+1; i++) {
+			prefix[i]=Integer.parseInt(st.nextToken())+prefix[i-1];
+		}
+		while(M-->0) {
+			st= new StringTokenizer(br.readLine());
+			int i=Integer.parseInt(st.nextToken());
+			int j=Integer.parseInt(st.nextToken());
+			System.out.println(prefix[j]-prefix[i-1]);
+		}
+	}
+}
